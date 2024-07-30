57e870d86ff0dca524cc2a6c80b8402933ba3370
ruxia chen <ruxiachen2008@gmail.com>
2024-07-30 10:50:56 -0400

    second commit

---
+++ 2ndQ.swift
@@ -0,0 +1,18 @@
+//
+//  2ndQ.swift
+//  quiz
+//
+//  Created by Scholar on 7/30/24.
+//
+
+import SwiftUI
+
+struct _ndQ: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    _ndQ()
+}
